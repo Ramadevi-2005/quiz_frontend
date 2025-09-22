d1598b63e740c27b378b19c0ffc08fceb94ffd23
Ramadevi-2005 <157826221+Ramadevi-2005@users.noreply.github.com>
2025-09-22 19:24:20 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/quizbuilder/"   // 👈 important for Tomcat subpath
+  base: '/quiz_front/',   // 👈 important for Tomcat subpath
 })
